34aaa63eb3112f61d45e699b6a3f4dfcb5442ea6
SergioCaueGomes <caue.sergio@gmail.com>
2022-10-14 22:32:18 -0300

    Exercicio proposto pela Prof. Camila

--- Collections/exerciciosCollections/ExemploList.java
+++ Collections/exerciciosCollections/ExemploList.java
@@ -1,9 +1,6 @@
 package exerciciosCollections;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
 
 public class ExemploList {
 
@@ -24,73 +21,101 @@ public class ExemploList {
         * System.out.println(notas);
         * */
 
-        System.out.println("Crie uma lista e adicione as sete notas: ");
-
-        List<Double> notas = new ArrayList<Double>();
-        notas.add(7.0);
-        notas.add(8.5);
-        notas.add(9.3);
-        notas.add(5.0);
-        notas.add(8.0);
-        notas.add(7.0);
-        notas.add(0.0);
-        notas.add(3.6);
-        System.out.println(notas.toString());
-
-        System.out.println("Exiba a posição da nota 5.0: "+ notas.indexOf(5d));
-
-        System.out.println("Adicione na lista a nota 8.0 na posição 4: ");
-        notas.add(4, 8d);
-        System.out.println(notas);
-
-
-        System.out.println("Substitua a nota 5.0 pela nota 6.0: ");
-        notas.set(notas.indexOf(5d), 6.0);
-        System.out.println(notas);
-
-        System.out.println("Confira se a nota 5.0 está na lista: "+ notas.contains(5d));
+//        System.out.println("Crie uma lista e adicione as sete notas: ");
+//
+//        List<Double> notas = new ArrayList<Double>();
+//        notas.add(7.0);
+//        notas.add(8.5);
+//        notas.add(9.3);
+//        notas.add(5.0);
+//        notas.add(8.0);
+//        notas.add(7.0);
+//        notas.add(0.0);
+//        notas.add(3.6);
+//        System.out.println(notas.toString());
+//
+//        System.out.println("Exiba a posição da nota 5.0: "+ notas.indexOf(5d));
+//
+//        System.out.println("Adicione na lista a nota 8.0 na posição 4: ");
+//        notas.add(4, 8d);
+//        System.out.println(notas);
+//
+//
+//        System.out.println("Substitua a nota 5.0 pela nota 6.0: ");
+//        notas.set(notas.indexOf(5d), 6.0);
+//        System.out.println(notas);
+//
+//        System.out.println("Confira se a nota 5.0 está na lista: "+ notas.contains(5d));
 
        /* System.out.println("Exiba todas as notas ba ordem em que foram informados: ");
         for (Double nota: notas) System.out.println(nota); */
 
-        System.out.println("Exiba a terceira nota adicionada: " + notas.get(2));
-        System.out.println(notas.toString());
+//        System.out.println("Exiba a terceira nota adicionada: " + notas.get(2));
+//        System.out.println(notas.toString());
+//
+//        System.out.println("Exiba a menor nota: " + Collections.min(notas));
+//
+//        System.out.println("Exiba a maior nota: " + Collections.max(notas));
+//
+//
+//        Iterator<Double> iterator = notas.iterator();
+//        Double soma = 0d;
+//        while (iterator.hasNext()) {
+//            Double next = iterator.next();
+//            soma += next;
+//        }
+//        System.out.println("Exiba a soma dos valores: " + soma);
+//
+//        System.out.println("Exiba a media das notas: "+ (soma/notas.size()));
+//
+//        System.out.println("Remova a nota 0: ");
+//        notas.remove(0d);
+//        System.out.println(notas);
+//
+//        System.out.println("Remova a nota da posição 0: ");
+//        notas.remove(0);
+//        System.out.println(notas);
+//
+//        System.out.println("Remova as notas menores que 7 e exiba a lista: ");
+//        Iterator<Double> iterator1 = notas.iterator();
+//        while(iterator1.hasNext()) {
+//            Double next = iterator1.next();
+//            if(next <  7) iterator1.remove();
+//        }
+//        System.out.println(notas);
+//
+//        System.out.println("Apague toda a lista");
+//        notas.clear();
+//        System.out.println(notas);
+//
+//        System.out.println("Confira se a lista está vazia: " + notas.isEmpty());
 
-        System.out.println("Exiba a menor nota: " + Collections.min(notas));
+//------------------------------------------------------------------------------------------------
+        //Resolva os exercicios utilizabdo os metodos da implementação LinkedList:
 
-        System.out.println("Exiba a maior nota: " + Collections.max(notas));
+        System.out.println("Crie uma lista chamada notas2 " + "e coloque todos os elementos do ArrayList nessa nova Lista: ");
+        LinkedList<Double> notas2 = new LinkedList<Double>();
+        notas2.add(7.0);
+        notas2.add(8.5);
+        notas2.add(9.3);
+        notas2.add(5.0);
+        notas2.add(8.0);
+        notas2.add(6.0);
+        notas2.add(0.0);
+        notas2.add(3.6);
+        System.out.println(notas2.toString());
+        System.out.println("");
+
+        System.out.println("Mostre a primeira nota da nova lista sem removê-lo: " + notas2.get(0));
+        System.out.println("");
+
+        System.out.println("Mostre a primeira nota da nova lista sem removendo-o: " + notas2.get(0));
+        notas2.remove(0);
+        System.out.println(notas2);
 
 
-        Iterator<Double> iterator = notas.iterator();
-        Double soma = 0d;
-        while (iterator.hasNext()) {
-            Double next = iterator.next();
-            soma += next;
-        }
-        System.out.println("Exiba a soma dos valores: " + soma);
 
-        System.out.println("Exiba a media das notas: "+ (soma/notas.size()));
 
-        System.out.println("Remova a nota 0: ");
-        notas.remove(0d);
-        System.out.println(notas);
 
-        System.out.println("Remova a nota da posição 0: ");
-        notas.remove(0);
-        System.out.println(notas);
-
-        System.out.println("Remova as notas menores que 7 e exiba a lista: ");
-        Iterator<Double> iterator1 = notas.iterator();
-        while(iterator1.hasNext()) {
-            Double next = iterator1.next();
-            if(next <  7) iterator1.remove();
-        }
-        System.out.println(notas);
-
-        System.out.println("Apague toda a lista");
-        notas.clear();
-        System.out.println(notas);
-
-        System.out.println("Confira se a lista está vazia: " + notas.isEmpty());
     }
 }
